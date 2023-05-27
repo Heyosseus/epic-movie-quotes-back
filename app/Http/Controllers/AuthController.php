459a23384bf6cac7d2a5941e595f6962b9403528
heyosseus <ratiruxadzee@gmail.com>
2023-05-27 14:44:20 +0400

    feat: add logout method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,22 +13,36 @@ class AuthController extends Controller
 {
 	public function register(AuthRegisterRequest $request) : object
 	{
-		$user = User::create($request->validated());
 
-		Mail::to($user->email)->send(new AccountActivationMail($user->token));
-		$user->email_verified_at = now();
-		return response()->json(['user' => $user], 200);
+        $attr = $request->validated();
+        $user = User::where('email', $attr['email'])->first();
+        $user->createToken('authToken')->plainTextToken;
+
+        if($user){
+            return response()->json(['message' => 'User already exists!'], 400);
+        }else{
+            $newUser = User::create($attr);
+            $token = $newUser->createToken('authToken')->plainTextToken;
+        }
+
+		Mail::to($newUser->email)->send(new AccountActivationMail($newUser));
+        $newUser->email_verified_at = now();
+		return response()->json(['user' => $newUser, 'token' => $token], 200);
 	}
 
 	public function login(AuthLoginRequest $request)
 	{
-        Auth::once($request->validated());
-
-        $result['Logged in'] = Auth::check();
-
-        if($result['Logged in']){
-            $result['token'] = Auth::user()->createToken('authToken')->plainTextToken;
+        $attrs = $request->validated();
+        $user = User::where('email', $attrs['email'])->first();
+        if(!$user){
+            return response()->json(['message' => 'User not found!'], 404);
         }
-        return $result;
+        $token = $user->createToken('authToken')->plainTextToken;
+        return response()->json(['user' => $user, 'token' => $token], 200);
 	}
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+        return response()->json(['message' => 'Logged out!'], 200);
+    }
 }
