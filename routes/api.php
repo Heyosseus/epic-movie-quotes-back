152e3308f711709f7a79e6f969b1cda344a5e4b1
heyosseus <ratiruxadzee@gmail.com>
2023-06-05 14:46:33 +0400

    feat: refactor routes in corresponding controller group

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\GoogleAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,22 +22,34 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 	Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
 });
 
-Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
-Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
-Route::post('/forgot-password', [App\Http\Controllers\RecoveryPasswordController::class, 'store']);
-Route::put('/reset-password', [App\Http\Controllers\RecoveryPasswordController::class, 'update']);
+Route::controller(\App\Http\Controllers\AuthController::class)->group(function () {
+	Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+	Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
+});
 
-Route::get('/auth/google/redirect', [GoogleAuthController::class, 'redirect'])->name('google-auth');
-Route::get('/auth/google/callback', [GoogleAuthController::class, 'callback'])->name('google-auth-callback');
+Route::controller(\App\Http\Controllers\RecoveryPasswordController::class)->group(function () {
+	Route::post('/forgot-password', [App\Http\Controllers\RecoveryPasswordController::class, 'store']);
+	Route::put('/reset-password', [App\Http\Controllers\RecoveryPasswordController::class, 'update']);
+});
+
+// google auth
+Route::controller(\App\Http\Controllers\GoogleAuthController::class)->group(function () {
+	Route::get('/auth/google/redirect', [App\Http\Controllers\GoogleAuthController::class, 'redirect'])->name('google-auth');
+	Route::get('/auth/google/callback', [App\Http\Controllers\GoogleAuthController::class, 'callback'])->name('google-auth-callback');
+});
 
 // movie routes
-Route::get('/movies', [App\Http\Controllers\MovieController::class, 'index']);
-Route::post('/add-movies', [App\Http\Controllers\MovieController::class, 'store']);
-Route::get('/movies/{id}', [App\Http\Controllers\MovieController::class, 'show']);
+Route::controller(\App\Http\Controllers\MovieController::class)->group(function () {
+	Route::get('/movies', [App\Http\Controllers\MovieController::class, 'index']);
+	Route::post('/add-movies', [App\Http\Controllers\MovieController::class, 'store']);
+	Route::get('/movies/{id}', [App\Http\Controllers\MovieController::class, 'show']);
+});
 
 // quote routes
-Route::get('/quotes/{movieId}', [App\Http\Controllers\QuotesController::class, 'index']);
-Route::post('/add-quotes', [App\Http\Controllers\QuotesController::class, 'store']);
+Route::controller(\App\Http\Controllers\QuotesController::class)->group(function () {
+	Route::get('/quotes/{movieId}', [App\Http\Controllers\QuotesController::class, 'index']);
+	Route::post('/add-quotes', [App\Http\Controllers\QuotesController::class, 'store']);
+});
 
 //profile
 Route::post('/profile', [App\Http\Controllers\ProfileController::class, 'update']);
