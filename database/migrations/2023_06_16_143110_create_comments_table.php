7a0b057c05dc90ce902860360010424623bc432e
heyosseus <ratiruxadzee@gmail.com>
2023-06-16 20:11:04 +0400

    feat: add foreign ids for likes and comments

--- database/migrations/2023_06_16_143110_create_comments_table.php
+++ database/migrations/2023_06_16_143110_create_comments_table.php
@@ -4,24 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('comments', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('comments', function (Blueprint $table) {
+			$table->id();
+			$table->foreignId('user_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+			$table->foreignId('quote_id')->constrained('quotes')->onDelete('cascade')->onUpdate('cascade');
+			$table->string('content');
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('comments');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('comments');
+	}
 };
