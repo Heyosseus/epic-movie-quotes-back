50a4f32ee5f0898d960e9f86c8093f35ff824916
heyosseus <ratiruxadzee@gmail.com>
2023-06-16 20:11:42 +0400

    wip: create empty comment controller

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class CommentsController extends Controller
 {
-    //
+	public function index()
+	{
+	}
+
+	public function store()
+	{
+	}
 }
